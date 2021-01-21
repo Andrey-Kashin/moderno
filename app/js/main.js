c611a8202cacd9d21c015a222bd4e77002ff75ef
Andrey-Kashin <kolbasyk@list.ru>
2021-01-22 02:15:56 +0300

    add product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,19 @@ $(function(){
 
 	var Mixer = mixitup ( '.product__items' );
 
+	$(".rate-star").rateYo({
+		rating: 5,
+		starWidth: '12px',
+		readOnly: true
+	});
+
+	$('.product-slider__items').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		dots: true,
+		arrows: false
+	});
+
 
 });
